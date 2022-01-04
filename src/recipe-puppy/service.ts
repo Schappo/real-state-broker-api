5819d93d08592e22642de44541e4ff4cc8c163e0
Felipe Schappo <felipe@gobacklog.com>
2022-01-04 19:53:04 -0300

    commit

--- src/recipe-puppy/service.ts
+++
@@ -1,8 +0,0 @@
-import { ApiResponse } from 'apisauce';
-import { ErrorResponse } from '../shared/exception/interfaces';
-import { RecipesPuppyResp } from './interface';
-import recipePuppyApi from './recipePuppyApi.config';
-
-export default async function getRecipePuppyData(query: object): Promise<ApiResponse<RecipesPuppyResp, ErrorResponse>> {
-  return await recipePuppyApi.client.get<RecipesPuppyResp, ErrorResponse>('/', query);
-}
