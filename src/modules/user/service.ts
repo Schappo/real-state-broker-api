a5a78593fdb2c1e3d5916e95e516b5e967201b0a
Felipe Schappo <felipeschappo.dev@gmail.com>
2022-01-10 17:34:19 -0300

    add redis in docker-compose

--- src/modules/user/service.ts
+++ src/modules/user/service.ts
@@ -8,42 +8,42 @@ import { MongoId } from '../../shared/types'
 export class UserService {
   private readonly repository: UserRepository = new UserRepository(UserModel)
 
-  async create (user: User): Promise<DocumentType<User>> {
+  async create(user: User): Promise<DocumentType<User>> {
     const createdUser = await this.repository.create(user)
     createdUser.password = undefined
     return createdUser
   }
 
-  async findAll (): Promise<DocumentType<User>[]> {
+  async findAll(): Promise<DocumentType<User>[]> {
     return await this.repository.findAll()
   }
 
-  async find (query: object): Promise<DocumentType<User>[]> {
+  async find(query: object): Promise<DocumentType<User>[]> {
     return await this.repository.find(query)
   }
 
-  async findById (id: MongoId): Promise<DocumentType<User>> {
+  async findById(id: MongoId): Promise<DocumentType<User>> {
     return await this.repository.findById(id)
   }
 
-  async findOne (query: object): Promise<DocumentType<User>> {
+  async findOne(query: object): Promise<DocumentType<User>> {
     return await this.repository.findOne(query)
   }
 
-  async findUserWithPassword (query: object): Promise<DocumentType<User>> {
+  async findUserWithPassword(query: object): Promise<DocumentType<User>> {
     return await this.repository.findOneWithPassword(query)
   }
 
-  async delete (id: ObjectId): Promise<boolean> {
+  async delete(id: ObjectId): Promise<boolean> {
     return Boolean(await this.repository.delete(id))
   }
 
-  async update (id: MongoId, newUser: User): Promise<DocumentType<User>> {
+  async update(id: MongoId, newUser: User): Promise<DocumentType<User>> {
     const user: User = await this.repository.findById(id)
 
     if (!user) throw new ApiError('Uer Not Found', 404)
 
-    await delete user.password
+    newUser.password = undefined
 
     return await this.repository.update(id, newUser)
   }
